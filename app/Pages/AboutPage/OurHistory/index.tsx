9c9104e299148e7c4d883921ccc500f0940e7e19
RadizJoster-web <radizdirganta0101@gmail.com>
2026-06-13 09:12:10 +0700

    fix: menambahkan id our-history di section our history

--- app/Pages/AboutPage/OurHistory/index.tsx
+++ app/Pages/AboutPage/OurHistory/index.tsx
@@ -5,7 +5,7 @@ import HistoryContent from "./HistoryContent";
 
 export default function OurHistory() {
   return (
-    <section className="relative w-full bg-[#FAF6F0] py-16 sm:py-24 lg:py-32 overflow-hidden">
+    <section id="our-history" className="relative w-full bg-[#FAF6F0] py-16 sm:py-24 lg:py-32 overflow-hidden">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         {/* Sistem Grid yang responsif, rapi, dan seimbang */}
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 lg:gap-20 items-center justify-items-center">
